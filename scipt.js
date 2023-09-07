2a00eeb965e4c64c24c02cc4c7e42c205ebb8235
mikejoshualopena44 <115403172+mikejoshualopena44@users.noreply.github.com>
2023-09-08 06:38:34 +0800

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,59 @@
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offeset + height) {
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+};
+
+function checkInputs() {
+  //trim to remove the whitespaces
+  const usernameValue = username.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+
+  if(usernameValue == '') {
+    setErrorFor(username, 'Username cannot be blank');
+  } else {
+    setSuccessFor(username);
+  }
+
+  if(passwordValue == '') {
+    setErrorFor(password, 'Password cannot be blank');
+  } else {
+    setSuccessFor(password);
+  }
+  if(password2Value == '') {
+    setErrorFor(password2, 'Passord2 cannot be blank');
+  } else if(passwordValue !== passwordValue) {
+    setErrorFor(password2, 'Passwords does not match');
+  } else {
+    setSuccessFor(password2);
+  }
+} 
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  formControl.className = 'form-control error';
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+
+}
+
