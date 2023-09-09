34d59e3ad9d9c966abd3b5e719a0012a1488dbcb
mikejoshualopena44 <115403172+mikejoshualopena44@users.noreply.github.com>
2023-09-10 00:29:14 +0800

    responsive navbar

--- scipt.js
+++ scipt.js
@@ -57,3 +57,18 @@ function setSuccessFor(input) {
 
 }
 
+
+//responsive
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'bx bx-x-circle'
+    : 'bx bx-menu'
+}
+
